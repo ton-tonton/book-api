97ec3975b30f8f0702fd24963255c28773614f4d
tontonton <phantom.ness@windowslive.com>
2016-09-13 14:49:18 +0700

    Use express router

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+.get(function(req, res) {
+  var responseJson = {hi: 'there.'};
+  res.json(responseJson);
+});
+
+app.use('/api', bookRouter);
+
 app.get('/', function(req, res) {
   res.send('bello everyone :)');
 });
