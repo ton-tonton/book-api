6070c4e679ee6f256397c70fe08508132af72a2d
tontonton <phantom.ness@windowslive.com>
2016-09-29 15:42:39 +0700

    Add post /books to create new book

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 
@@ -9,9 +10,19 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const bookRouter = express.Router();
 
 bookRouter.route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+    if (book.save()) {
+      res.status(201).send(book);
+    }
+    return false;
+  })
   .get((req, res) => {
     const query = req.query;
     Book.find(query, (err, books) => {
